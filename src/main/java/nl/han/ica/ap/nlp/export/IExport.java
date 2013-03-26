33c8baaf92ff3d18f6b8fb1297c23f0bcaa5f31d
Michiel Westerbeek <happylinks@gmail.com>
2013-03-26 15:06:25 +0100

    Added an export-function to the IExport interface

--- src/main/java/nl/han/ica/ap/nlp/export/IExport.java
+++ src/main/java/nl/han/ica/ap/nlp/export/IExport.java
@@ -1,5 +1,9 @@
 package nl.han.ica.ap.nlp.export;
 
-public interface IExport {
+import java.util.ArrayList;
 
+import nl.han.ica.ap.nlp.model.Class;
+
+public interface IExport {
+	String export(ArrayList<Class> classes);
 }
