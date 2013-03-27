5b0fe57ca2a96459541fd3b51c03bc5b0e3097f4
Michiel Westerbeek <happylinks@gmail.com>
2013-03-27 10:34:19 +0100

    Changed export encoding to UTF-8.

--- src/main/java/nl/han/ica/ap/nlp/export/PowerDesignerExport.java
+++ src/main/java/nl/han/ica/ap/nlp/export/PowerDesignerExport.java
@@ -1,5 +1,12 @@
 package nl.han.ica.ap.nlp.export;
 
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.io.Writer;
 import java.util.ArrayList;
 
 import nl.han.ica.ap.nlp.model.Class;
@@ -7,14 +14,21 @@ import nl.han.ica.ap.nlp.model.Class;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
+import org.w3c.dom.DOMImplementation;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.ls.DOMImplementationLS;
+import org.w3c.dom.ls.LSOutput;
 import org.w3c.dom.ls.LSSerializer;
 
 public class PowerDesignerExport implements IExport {
 	
-    public String export(ArrayList<Class> classes){
+	public String export(ArrayList<Class> classes){
+		String filepath = "target/Powerdesigner-xml-" + (System.currentTimeMillis()/1000) + ".xml";
+		return export(classes, filepath);
+	}
+	
+    public String export(ArrayList<Class> classes, String filepath){
     	
     	// Create a new document.
 	    Document doc = null;
@@ -27,12 +41,11 @@ public class PowerDesignerExport implements IExport {
 	    
 	    // Create and add a root element and an attribute.
 	    Element root = doc.createElement("uml:Model");
-	    doc.appendChild(root);
+	    root.setAttribute("name", "ObjectOrientedModel");	    
 	    root.setAttribute("xmi:version", "2.1");
 	    root.setAttribute("xmlns:xmi", "http://schema.omg.org/spec/XMI/2.1");
 	    root.setAttribute("xmlns:uml", "http://www.eclipse.org/uml2/2.1.0/UML");
-	    root.setAttribute("xmi:id", "MODEL1");
-	    root.setAttribute("name", "ObjectOrientedModel");
+	    doc.appendChild(root);
     	
 	    Element packagedElement = null;
 	    
@@ -45,10 +58,26 @@ public class PowerDesignerExport implements IExport {
 		}
 		
 		// Output the document.
-	    DOMImplementationLS domImplementationLS = (DOMImplementationLS) doc.getImplementation();
-        LSSerializer lsSerializer = domImplementationLS.createLSSerializer();
-        String output = lsSerializer.writeToString(doc);
+	    DOMImplementation impl = doc.getImplementation();
+	    DOMImplementationLS implLS = (DOMImplementationLS) impl.getFeature("LS", "3.0");
+        LSSerializer lsSerializer = implLS.createLSSerializer();
         
-        return output;
+        LSOutput lsOutput = implLS.createLSOutput();
+        lsOutput.setEncoding("UTF-8");
+        Writer stringWriter = new StringWriter();
+        lsOutput.setCharacterStream(stringWriter);
+        lsSerializer.write(doc, lsOutput);
+        
+        String output = stringWriter.toString();
+        
+        try {
+            BufferedWriter out = new BufferedWriter(new FileWriter(filepath));
+            out.write(output);
+            out.close();
+        }catch (IOException e){ 
+           System.out.println("Can't write file.");
+        }
+        
+        return filepath;
 	}
 }
