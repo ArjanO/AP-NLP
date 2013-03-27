e53758f4200dacc44550fb7cfb15e5417d8736a7
Michiel Westerbeek <happylinks@gmail.com>
2013-03-27 10:58:18 +0100

    Added comments to IExport

--- src/main/java/nl/han/ica/ap/nlp/export/IExport.java
+++ src/main/java/nl/han/ica/ap/nlp/export/IExport.java
@@ -5,5 +5,11 @@ import java.util.ArrayList;
 import nl.han.ica.ap.nlp.model.Class;
 
 public interface IExport {
+	
+	/**
+	 * Exports NLP model to other format.
+	 * @param classes List of classes.
+	 * @return path of exported file or empty string if error.
+	 */
 	String export(ArrayList<Class> classes);
 }
