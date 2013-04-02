4a6432bb6bbbb59250635682254c3f41a612cc2a
Michiel Westerbeek <happylinks@gmail.com>
2013-04-02 10:04:49 +0200

    Changed test for associations

--- src/test/java/nl/han/ica/ap/nlp/export/PowerDesignerExportTest.java
+++ src/test/java/nl/han/ica/ap/nlp/export/PowerDesignerExportTest.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 
 import nl.han.ica.ap.nlp.export.PowerDesignerExport;
 import nl.han.ica.ap.nlp.model.Class;
+import nl.han.ica.ap.nlp.model.IClass;
 
 import org.codehaus.plexus.util.IOUtil;
 import org.junit.Test;
@@ -19,11 +20,11 @@ public class PowerDesignerExportTest extends TestCase{
 	public void testExport() {
 		PowerDesignerExport exporter = new PowerDesignerExport();
 		
-		ArrayList<Class> classes = new ArrayList<Class>();
-		Class vliegtuig = new Class("Vliegtuig");
-		Class passagier = new Class("Passagier");
+		ArrayList<IClass> classes = new ArrayList<IClass>();
+		IClass vliegtuig = new Class("Vliegtuig");
+		IClass passagier = new Class("Passagier");
+		vliegtuig.getAttributes().add(passagier);
 		classes.add(vliegtuig);
-		classes.add(passagier);
 		
 		String exportpath = exporter.export(classes);
 		
@@ -50,7 +51,7 @@ public class PowerDesignerExportTest extends TestCase{
 			}
 	    }
 		
-		String expected_output = "<?xmlversion=\"1.0\"encoding=\"UTF-8\"?><uml:Modelname=\"ObjectOrientedModel\"xmi:version=\"2.1\"xmlns:uml=\"http://www.eclipse.org/uml2/2.1.0/UML\"xmlns:xmi=\"http://schema.omg.org/spec/XMI/2.1\"><packagedElementname=\"Vliegtuig\"xmi:id=\"VLIEGTUIG\"xmi:type=\"uml:Class\"/><packagedElementname=\"Passagier\"xmi:id=\"PASSAGIER\"xmi:type=\"uml:Class\"/></uml:Model>";
+		String expected_output = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><uml:Model name=\"ObjectOrientedModel\" xmi:version=\"2.1\" xmlns:uml=\"http://www.eclipse.org/uml2/2.1.0/UML\" xmlns:xmi=\"http://schema.omg.org/spec/XMI/2.1\"><packagedElement name=\"Vliegtuig\" xmi:id=\"VLIEGTUIG\" xmi:type=\"uml:Class\"/><packagedElement name=\"Passagier\" xmi:id=\"PASSAGIER\" xmi:type=\"uml:Class\"/><packagedElement memberEnd=\"OWNEDEND_VLIEGTUIG OWNEDEND_PASSAGIER\" name=\"Association_TEST\" navigableOwnedEnd=\"OWNEDEND_PASSAGIER\" xmi:id=\"ASSOCIATION_TEST\" xmi:type=\"uml:Association\"><ownedEnd association=\"ASSOCIATION_VLIEGTUIG\" name=\"\" type=\"VLIEGTUIG\" visibility=\"public\" xmi:id=\"OWNEDEND_VLIEGTUIG\"><upperValue value=\"1\" xmi:id=\"UPPERVALUE_VLIEGTUIG\" xmi:type=\"uml:LiteralUnlimitedNatural\"/><lowerValue xmi:id=\"LOWERVALUE_VLIEGTUIG\" xmi:type=\"LiteralInteger\"/></ownedEnd><ownedEnd association=\"ASSOCIATION_PASSAGIER\" name=\"\" type=\"PASSAGIER\" visibility=\"public\" xmi:id=\"OWNEDEND_PASSAGIER\"><upperValue value=\"1\" xmi:id=\"UPPERVALUE_PASSAGIER\" xmi:type=\"uml:LiteralUnlimitedNatural\"/><lowerValue xmi:id=\"LOWERVALUE_PASSAGIER\" xmi:type=\"LiteralInteger\"/></ownedEnd></packagedElement></uml:Model>";
 		
 		output = output.replaceAll("\\s","");
 		expected_output = expected_output.replaceAll("\\s","");
