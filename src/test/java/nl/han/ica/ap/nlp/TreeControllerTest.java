4bdd3a35ab24f2dfa7f78dbd9c31e7dd93a1d51b
Sjoel <joellportier@gmail.com>
2013-04-11 13:27:46 +0200

    added testcase for scenario 4: A class already exists as class, but the new class-attribute pair has a new attribute. The attribute will be moved to the already existing class

--- src/test/java/nl/han/ica/ap/nlp/TreeControllerTest.java
+++ src/test/java/nl/han/ica/ap/nlp/TreeControllerTest.java
@@ -54,5 +54,38 @@ public class TreeControllerTest {
 		assertEquals(passagier,actualclass.getAttributes().get(0));
 	}
 	
+	@Test
+	public void testClassesShareAttribute() {
+		TreeController controller = new TreeController();
+		Class vliegtuig = new Class("vliegtuig");
+		Class passagier1 = new Class("passagier");
+		vliegtuig.addAttribute(passagier1);
+		controller.addClass(vliegtuig);
+		Class bus = new Class("bus");
+		Class passagier2 = new Class("passagier");
+		bus.addAttribute(passagier2);
+		controller.addClass(bus);
+		assertEquals(2, controller.classes.size());
+		assertEquals(controller.classes.get(0).getAttributes().get(0), controller.classes.get(1).getAttributes().get(0));
+	}
+	
+	@Test
+	public void testAlreadyExistsAsClass() {
+		TreeController controller = new TreeController();
+		Class vliegtuig = new Class("vliegtuig");
+		Class passagier = new Class("passagier");
+		vliegtuig.addAttribute(passagier);
+		controller.addClass(vliegtuig);
+		Class vliegtuig2 = new Class("vliegtuig");
+		Class piloot = new Class("piloot");
+		vliegtuig2.addAttribute(piloot);
+		controller.addClass(vliegtuig2);
+		assertEquals(1, controller.classes.size());
+		assertEquals(2, controller.classes.get(0).getAttributes().size());
+		assertEquals(vliegtuig,controller.classes.get(0));
+		assertEquals(passagier,controller.classes.get(0).getAttributes().get(0));
+		assertEquals(piloot,controller.classes.get(0).getAttributes().get(1));
+	}
+	
 	
 }
