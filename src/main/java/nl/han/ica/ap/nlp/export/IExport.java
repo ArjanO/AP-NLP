82cdc1c52d9b52a2ce7b12840570d5d00c5c874f
Michiel Westerbeek <happylinks@gmail.com>
2013-04-18 14:03:35 +0200

    Change parameter to arraylist

--- src/main/java/nl/han/ica/ap/nlp/export/IExport.java
+++ src/main/java/nl/han/ica/ap/nlp/export/IExport.java
@@ -32,8 +32,7 @@ package nl.han.ica.ap.nlp.export;
 import java.util.ArrayList;
 import java.util.TreeMap;
 
-import nl.han.ica.ap.nlp.model.IClass;
-import nl.han.ica.ap.nlp.model.Multiplicity;
+import nl.han.ica.ap.nlp.model.Class;
 
 public interface IExport {
 	
@@ -42,5 +41,5 @@ public interface IExport {
 	 * @param classes List of classes.
 	 * @return path of exported file or empty string if error.
 	 */
-	String export(ArrayList<IClass> classes);
+	String export(ArrayList<Class> classes);
 }
