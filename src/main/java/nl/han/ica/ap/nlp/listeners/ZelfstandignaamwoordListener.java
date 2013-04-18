690175ecfb9203203b5b49fd74ac7d2e9b2530e9
Joëll Portier <joellportier@gmail.com>
2013-04-18 14:12:22 +0200

    Fixed to be compatible with new class structure

--- src/main/java/nl/han/ica/ap/nlp/listeners/ZelfstandignaamwoordListener.java
+++ src/main/java/nl/han/ica/ap/nlp/listeners/ZelfstandignaamwoordListener.java
@@ -29,18 +29,10 @@
  */
 package nl.han.ica.ap.nlp.listeners;
 
-import java.util.ArrayList;
-
-import nl.han.ica.ap.nlp.App;
 import nl.han.ica.ap.nlp.controller.TreeController;
 import nl.han.ica.ap.nlp.controller.VerbDirectionController;
 import nl.han.ica.ap.nlp.model.Class;
-import nl.han.ica.ap.nlp.model.IAttribute;
-import nl.han.ica.ap.nlp.model.IClass;
-import nl.han.ica.ap.nlp.model.Multiplicity;
 import nl.han.ica.ap.nlp.NlpBaseListener;
-import nl.han.ica.ap.nlp.NlpParser;
-import nl.han.ica.ap.nlp.NlpParser.BijwoordContext;
 import nl.han.ica.ap.nlp.NlpParser.WerkwoordContext;
 import nl.han.ica.ap.nlp.NlpParser.ZelfstandignaamwoordContext;
 
@@ -59,19 +51,11 @@ public class ZelfstandignaamwoordListener extends NlpBaseListener {
 		if(start) {
 			if(!direction) {
 				Class c = new Class(znw.getText());
-				IAttribute a = new Class(ctx.getText());
-				Multiplicity[] m = new Multiplicity[2];
-				m[0] = new Multiplicity();
-				m[1] = new Multiplicity();
-				c.addAttribute(a,m);
+				c.addAssociation(new Class(ctx.getText()));					
 				controller.addClass(c);
 			} else {
 				Class c = new Class(ctx.getText());
-				IAttribute a = new Class(znw.getText());
-				Multiplicity[] m = new Multiplicity[2];
-				m[0] = new Multiplicity();
-				m[1] = new Multiplicity();
-				c.addAttribute(a,m);
+				c.addAssociation(new Class(znw.getText()));	
 				controller.addClass(c);
 			}
 			start = false;
